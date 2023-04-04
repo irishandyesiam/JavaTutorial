e4b77756b1996147d1349495b8bf507edc607dad
irishandyesiam <nathan.irish@gmail.com>
2023-04-03 23:29:03 -0500

    Installed eclipse and set up workspace for tutorial.

---
+++ src/tutorial/Main.java
@@ -0,0 +1,15 @@
+package tutorial;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int scanned = sc.nextInt();
+		
+		
+		System.out.println(scanned);
+	}
+
+}
