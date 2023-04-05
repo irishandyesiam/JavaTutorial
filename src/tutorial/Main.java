f005813aa6583d2f9f2afad201b84e46cfc76e44
irishandyesiam <nathan.irish@gmail.com>
2023-04-05 15:40:04 -0500

    For each loop with count.

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -6,13 +6,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr = {1,5,7,3,4,5};
+		String[] names = new String[5];
 		
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == 5) {
-				System.out.println("Found a 5!");
-			}
-			else System.out.println("Not a five!");
+		int count = 0;
+		for (int element:arr) {
+			System.out.println(count + " " + element);
+			count++;
 		}
+		
 	}
 
 }
