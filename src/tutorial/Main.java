794142262241264ea3ec53501c635ba2061e4707
irishandyesiam <nathan.irish@gmail.com>
2023-04-05 16:37:50 -0500

    Do/while loop

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -6,17 +6,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Type a number: ");
-		int x = sc.nextInt();
+		int x;
 		
-		int count = 0;
-		while (x != 10) {
-			System.out.println("Type 10...");
+		do {
 			System.out.print("Type a number: ");
 			x = sc.nextInt();
-			count++;
-		}
-		System.out.println("You tried " + count + " times");
+		} while (x != 10);
 	}
 
 }
