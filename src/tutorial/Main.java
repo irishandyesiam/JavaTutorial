3b44262e054c83d2244fd9e4de60c7671b31deb7
irishandyesiam <nathan.irish@gmail.com>
2023-04-05 16:32:16 -0500

    While loops

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -5,21 +5,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = {1,5,7,3,4,5};
-		String[] names = new String[5];
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Type a number: ");
+		int x = sc.nextInt();
 		
-		for (int i =0;i < names.length; i++) {
-			System.out.println("input: ");
-			String input = sc.nextLine();
-			names[i] = input;
-		}
-		
-		for (String n:names) {
-			System.out.println(n);
-			if(n.equals("tim")) {
-				break;
-			}
+		while (x != 10) {
+			System.out.println("Type 10...");
+			System.out.print("Type a number: ");
+			x = sc.nextInt();
 		}
 	}
 
