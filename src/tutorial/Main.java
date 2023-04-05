3cbf38b4fc6942ad453a28b6fe336245516a81f6
irishandyesiam <nathan.irish@gmail.com>
2023-04-05 15:21:49 -0500

    Example of arrays

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -5,30 +5,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Input your age: ");
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		int age = Integer.parseInt(s);
+		String[] newArr = new String[5];	
+		newArr[0] = "hello";
+		newArr[1] = "hi";
+		newArr[2] = "tim";
+		newArr[3] = "bill";
+		newArr[4] = "joe";
 		
-		if (age >= 18 ) {
-			System.out.print("Input your fav food: ");
-			String food = sc.nextLine();
-			
-			if (food.equals("pizza")) {
-				System.out.println("Mine too");
-			}
-			else {
-				System.out.println("Not mine");
-			}
-		}
-		else if (age >= 13) {
-			System.out.println("You are a teenager.");
-		}
+		int[] nums = {2,3,54,6,6};
 		
-		else {
-			System.out.println("You are a not a teenager or an adult.");
-		}
+		double[] nums2 = {2.0,3.0};
 		
+		double x = nums2[0];
+		System.out.println(x);
 		
 	}
 
