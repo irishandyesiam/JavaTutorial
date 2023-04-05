95fd310c36814ab83e4f734026fb5c3f39c986b8
irishandyesiam <nathan.irish@gmail.com>
2023-04-05 16:34:38 -0500

    While loop with counter.

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -9,11 +9,14 @@ public class Main {
 		System.out.print("Type a number: ");
 		int x = sc.nextInt();
 		
+		int count = 0;
 		while (x != 10) {
 			System.out.println("Type 10...");
 			System.out.print("Type a number: ");
 			x = sc.nextInt();
+			count++;
 		}
+		System.out.println("You tried " + count + " times");
 	}
 
 }
