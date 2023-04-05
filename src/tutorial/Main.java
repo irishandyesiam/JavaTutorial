2e991eef3d907edc78a68c53eabbfa3f75e71835
irishandyesiam <nathan.irish@gmail.com>
2023-04-05 15:32:07 -0500

    For loops with if/else.

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -5,20 +5,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] newArr = new String[5];	
-		newArr[0] = "hello";
-		newArr[1] = "hi";
-		newArr[2] = "tim";
-		newArr[3] = "bill";
-		newArr[4] = "joe";
+		int[] arr = {1,5,7,3,4,5};
 		
-		int[] nums = {2,3,54,6,6};
-		
-		double[] nums2 = {2.0,3.0};
-		
-		double x = nums2[0];
-		System.out.println(x);
-		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 5) {
+				System.out.println("Found a 5!");
+			}
+			else System.out.println("Not a five!");
+		}
 	}
 
 }
