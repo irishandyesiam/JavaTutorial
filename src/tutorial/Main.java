1f040e99164dac087a133cc10adc93730a9fabc7
irishandyesiam <nathan.irish@gmail.com>
2023-04-06 13:38:49 -0500

    Hashmap

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -4,26 +4,22 @@ import java.util.Set;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<Integer> t = new ArrayList<Integer>();
-		//LinkedList??? Faster at certain opperations.
-		t.add(1);
-		t.add(2);
-		t.add(1);
-		t.add(2);
-		t.add(1);
-		t.add(2);
-		t.get(0);
-		t.set(1, 5);
-		//t.size();
-		//t.Empty();
-		t.subList(1, 4);
+		// key, value
+		Map m = new HashMap();
+		m.put("tim", 5);
+		m.put("joe", "x");
+		m.put(11, 999);
+		//overwrite m.put(11, 998);
 		
-		System.out.println(t.subList(1, 4));
+		System.out.println(m);
+		System.out.println(m.get(11));
 	}
 	
 }
