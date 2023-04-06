4f68ef4d765b3bf8ef6bf6e263677ed03ba4e976
irishandyesiam <nathan.irish@gmail.com>
2023-04-06 12:12:56 -0500

    Set, HashSet, TreeSet, and LinkedHashSet. Will need to explore this more.

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -1,17 +1,29 @@
 package tutorial;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.TreeSet;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int x;
 		
-		do {
-			System.out.print("Type a number: ");
-			x = sc.nextInt();
-		} while (x != 10);
+		//Set, HashSet, TreeSet, LinkedHashSet
+		
+		Set<Integer> t = new TreeSet<Integer>();
+		t.add(5);
+		t.add(7);
+		t.add(9);
+		t.add(5);
+		t.add(-8);
+		t.remove(9);
+		t.isEmpty();
+		t.clear();
+		
+		boolean x = t.isEmpty();
+		
+		System.out.println(t);
 	}
-
+	
 }
