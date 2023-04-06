8c4a0191f60df4e3e3fd57f6197e3187cbc11fe9
irishandyesiam <nathan.irish@gmail.com>
2023-04-06 13:42:19 -0500

    TreeMap

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -6,20 +6,20 @@ import java.util.HashSet;
 import java.util.TreeSet;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// key, value
-		Map m = new HashMap();
+		// TreeMap - key data types must be same 
+		Map m = new TreeMap();
 		m.put("tim", 5);
 		m.put("joe", "x");
-		m.put(11, 999);
-		//overwrite m.put(11, 998);
+		m.put("11", 999);
 		
 		System.out.println(m);
-		System.out.println(m.get(11));
+	
 	}
 	
 }
