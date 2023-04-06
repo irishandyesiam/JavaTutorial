660ff1d36efcfbe801fc8e139e7848288008367f
irishandyesiam <nathan.irish@gmail.com>
2023-04-06 13:44:39 -0500

    LinkedHashMap.

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -7,13 +7,14 @@ import java.util.TreeSet;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.LinkedHashMap;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// TreeMap - key data types must be same 
-		Map m = new TreeMap();
+		// LinkedHashMap - keeps order of keys as elements are added
+		Map m = new LinkedHashMap();
 		m.put("tim", 5);
 		m.put("joe", "x");
 		m.put("11", 999);
