2ecea0d74b5532ffad74cf1ff741accd0ade6771
irishandyesiam <nathan.irish@gmail.com>
2023-04-10 22:09:58 -0500

    Methods to set and/or change lvlNum

--- src/tutorial/Level.java
+++ src/tutorial/Level.java
@@ -2,7 +2,22 @@ package tutorial;
 
 public enum Level {
 //define constants
-	HIGH,
-	MEDIUM,
-	LOW;
+	HIGH(3),
+	MEDIUM(2),
+	LOW(1);
+	
+	private int lvlNum;
+	
+	private Level(int num) {
+		this .lvlNum = num;
+	}
+	
+	public int getLvl() {
+		return this.lvlNum;
+	}
+	
+	public void setLvl(int num) {
+		this.lvlNum = num;
+	}
+	
 }
