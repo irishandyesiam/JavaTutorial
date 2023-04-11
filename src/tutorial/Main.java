a99520e9d9286e97ec0db3af05a80ed9e08edfa7
irishandyesiam <nathan.irish@gmail.com>
2023-04-10 22:14:09 -0500

    Get and Set lvl display.

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -14,25 +14,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Level lvl = Level.HIGH;
-		//To convert to string example: String en = lvl.toString();
-		//									System.out.println(en);
+		Level lvl = Level.LOW;
+		System.out.println(lvl.getLvl());
+		//System.out.println(Level.valueOf("LOW"));
 		
-		//Create an arr to hold ALL values of Enum and display
-		Level[] arr = Level.values();
-		for (Level e : arr) {
-			System.out.println(e);
-		}
-		//System.out.println(Level.values());  This will print out the RAM address
-		
-		//check what value constant is
-		if (lvl == Level.LOW) {
-			System.out.println(lvl);
-		}else if (lvl == Level.MEDIUM) {
-			System.out.println(lvl);
-		}else {
-			System.out.println(lvl);
-		}
+		lvl.setLvl(5);
+		System.out.println(lvl.getLvl());
 	}
 	
 	
