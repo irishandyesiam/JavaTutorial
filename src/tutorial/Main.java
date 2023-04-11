ba150160b9b6cb5e125d04b722206127b9fa4d31
irishandyesiam <nathan.irish@gmail.com>
2023-04-10 22:05:54 -0500

    How to print out to console the various values of Enum.

--- src/tutorial/Main.java
+++ src/tutorial/Main.java
@@ -14,7 +14,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Level lvl = Level.LOW;
+		Level lvl = Level.HIGH;
+		//To convert to string example: String en = lvl.toString();
+		//									System.out.println(en);
+		
+		//Create an arr to hold ALL values of Enum and display
+		Level[] arr = Level.values();
+		for (Level e : arr) {
+			System.out.println(e);
+		}
+		//System.out.println(Level.values());  This will print out the RAM address
 		
 		//check what value constant is
 		if (lvl == Level.LOW) {
